fd878ab3d174070b0eab006c0608b1180a3a5e71
Meera Mendhe <meerasmendhe@gmail.com>
2026-05-01 13:26:56 +1000

    Created basic Frontend and Backend

---
+++ Frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+const App=()=>{
+    return <div>
+        <h1>Frontend</h1>
+    </div>
+}
+root.render(<App/>)
